128c4794770b499bb7b3676f2569ec15b62ba15e
Petko D. Petkov <pdp@gnucitizen.org>
2019-03-04 06:06:38 +0000

    Added iterateOverEmitter method.

---
+++ test/iterateOverEmitter.js
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const { EventEmitter } = require('events')
+
+const { iterateOverEmitter } = require('../lib/iterateOverEmitter')
+
+describe('iterateOverEmitter', () => {
+    it('produces the correct numbers', async() => {
+        const ee = new EventEmitter()
+
+        setTimeout(() => {
+            ee.emit('item', 0)
+            ee.emit('item', 1)
+            ee.emit('item', 2)
+            ee.emit('end')
+        }, 1)
+
+        const items = []
+
+        for await (const item of iterateOverEmitter(ee, { yieldEvent: 'item' })) {
+            items.push(item)
+        }
+
+        items.sort()
+
+        assert.deepEqual(items, [0, 1, 2], 'items are 0, 1, 2')
+    })
+})
