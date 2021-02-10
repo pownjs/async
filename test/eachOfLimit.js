fa92ad55cacb400e33c84bab8a991b3e091c48b3
Petko D. Petkov <pdp@websecurify.com>
2021-02-10 19:38:10 +0000

    Minor patch to handle edge cases.

--- test/eachOfLimit.js
+++ test/eachOfLimit.js
@@ -30,4 +30,26 @@ describe('eachOfLimit', () => {
 
         assert.equal(items.length, 3, 'items.length is correct value')
     }).timeout(1000)
+
+    it('fetch tests', async() => {
+        const items = []
+
+        await eachOfLimit([0], 1, async(item) => {
+            await sleep(100)
+
+            items.push(item)
+        })
+
+        assert.equal(items.length, 1, 'items.length is correct value')
+        assert.equal(items[0], 0, 'items[0] is correct value')
+
+        await eachOfLimit([1], 2, async(item) => {
+            await sleep(100)
+
+            items.push(item)
+        })
+
+        assert.equal(items.length, 2, 'items.length is correct value')
+        assert.equal(items[1], 1, 'items[2] is correct value')
+    }).timeout(1000)
 })
