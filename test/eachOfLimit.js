08475bd449eef5dade32bbe67ed82f86d59da43d
Petko D. Petkov <pdp@websecurify.com>
2021-02-10 17:13:34 +0000

    Better tests.

--- test/eachOfLimit.js
+++ test/eachOfLimit.js
@@ -13,9 +13,21 @@ describe('eachOfLimit', () => {
             items.push(item)
         })
 
-        assert.equal(items.length, 3)
-        assert.equal(items[0], 0)
-        assert.equal(items[1], 1)
-        assert.equal(items[2], 2)
+        assert.equal(items.length, 3, 'items.length is correct value')
+        assert.equal(items[0], 0, 'items[0] is correct value')
+        assert.equal(items[1], 1, 'items[1] is correct value')
+        assert.equal(items[2], 2, 'itesm[2] is correct value')
+    }).timeout(1000)
+
+    it('fetches all at the same time', async() => {
+        const items = []
+
+        await eachOfLimit([0, 1, 2], 3, async(item) => {
+            await sleep((3 - item) * 100)
+
+            items.push(item)
+        })
+
+        assert.equal(items.length, 3, 'items.length is correct value')
     }).timeout(1000)
 })
