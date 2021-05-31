0751935193b389e555888087d7b26fa53d6a45db
Petko D. Petkov <pdp@websecurify.com>
2021-05-31 09:41:44 +0000

    Handle errors gracefully.

--- test/eachOfLimit.js
+++ test/eachOfLimit.js
@@ -66,4 +66,25 @@ describe('eachOfLimit', () => {
         assert.equal(items[0], 1, 'items[0] is correct value')
         assert.equal(items[9], 0, 'items[9] is correct value')
     }).timeout(2000)
+
+    it('with error', async() => {
+        const items = []
+
+        let error
+
+        try {
+            await eachOfLimit([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 2, async(item) => {
+                if (item === 5) {
+                    throw new Error('test')
+                }
+
+                items.push(item)
+            })
+        }
+        catch (e) {
+            error = e
+        }
+
+        assert.ok(error, 'error registered')
+    }).timeout(2000)
 })
